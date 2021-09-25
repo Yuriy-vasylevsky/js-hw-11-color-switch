5bdadd2b233edce4a9dbe29a2a68f2a7682791b1
Yuriy-vasylevsky <yuriy.vasylevsky@gmail.com>
2021-09-25 10:57:33 +0300

    зробив завдання іншим методом

--- src/index.js
+++ src/index.js
@@ -4,30 +4,54 @@ const startBtn = document.querySelector(`button[data-action="start"]`);
 const stopBtn = document.querySelector(`button[data-action="stop"]`);
 const body = document.querySelector(`body`);
 
-class ColorSwitch {
-  constructor({}) {
-    this.intervalid = null;
-    this.isActive = false;
-  }
-  startChangeColor() {
-    if (this.isActive) {
-      return;
-    }
-    const randomIntegerFromInterval = (min, max) => {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-    this.intervalid = setInterval(() => {
-      this.isActive = true;
-      let index = randomIntegerFromInterval(0, 5);
-      body.style.backgroundColor = colors[index];
-    }, 1000);
-  }
-  stopChangeColor() {
-    this.isActive = false;
-    clearTimeout(this.intervalid);
-  }
-}
-const colorSwitch = new ColorSwitch({});
+// class ColorSwitch {
+//   constructor({}) {
+//     this.intervalid = null;
+//     this.isActive = false;
+//   }
+//   startChangeColor() {
+//     if (this.isActive) {
+//       return;
+//     }
+//     const randomIntegerFromInterval = (min, max) => {
+//       return Math.floor(Math.random() * (max - min + 1) + min);
+//     };
+//     this.intervalid = setInterval(() => {
+//       this.isActive = true;
+//       let index = randomIntegerFromInterval(0, colors.length - 1);
+//       body.style.backgroundColor = colors[index];
+//     }, 1000);
+//   }
+//   stopChangeColor() {
+//     this.isActive = false;
+//     clearTimeout(this.intervalid);
+//   }
+// }
+// const colorSwitch = new ColorSwitch({});
 
-startBtn.addEventListener(`click`, colorSwitch.startChangeColor.bind(colorSwitch));
-stopBtn.addEventListener(`click`, colorSwitch.stopChangeColor.bind(colorSwitch));
+// startBtn.addEventListener(`click`, colorSwitch.startChangeColor.bind(colorSwitch));
+// stopBtn.addEventListener(`click`, colorSwitch.stopChangeColor.bind(colorSwitch));
+
+// варіант 2
+
+let intervalid = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function startChangeColor() {
+  intervalid = setInterval(() => {
+    let index = randomIntegerFromInterval(0, colors.length - 1);
+    body.style.backgroundColor = colors[index];
+  }, 1000);
+  startBtn.setAttribute('disabled', 'true');
+}
+
+function stopChangeColor() {
+  startBtn.removeAttribute('disabled');
+  clearTimeout(intervalid);
+}
+
+startBtn.addEventListener(`click`, startChangeColor);
+stopBtn.addEventListener(`click`, stopChangeColor);
